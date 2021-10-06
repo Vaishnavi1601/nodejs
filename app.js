9f01923f54405ed1b64794c21f87cb605c519b0a
Vaishnavi1601 <vaishnavijaiswal.18cs@saividya.ac.in>
2021-10-07 00:27:12 +0530

    creating dummy user

--- app.js
+++ app.js
@@ -15,14 +15,25 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const Product = require("./models/product");
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1) // find the user with user id 1
+    .then((user) => {
+      req.user = user; //adding new field to request object -- sequelize object
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -33,23 +44,28 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 Order.belongsTo(User);
 User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem})
+Order.belongsToMany(Product, { through: OrderItem });
 
+//this sets up our database
 sequelize
   .sync()
-
   .then((result) => {
-    app.listen(3000); 
+    return User.findByPk(1); //check if user already exit
   })
-
+  .then((user) => {
+    // if user doesnt exist create new user
+    if (!user) {
+      return User.create({ name: "abc", email: "abc@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    app.listen(3000);
+  })
   .catch((err) => {
     console.log(err);
-  }); //sync method keep track of all the models we define and then basically creates table for them
-
-
-
-  
+  });
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
     .then((result) => {
       console.log("Created Product");
       res.redirect("/admin/products");
@@ -32,28 +33,28 @@ exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   console.log(editMode);
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   const prodId = req.params.productId;
   console.log(prodId);
   // req.user
-    Product.findByPk(prodId)
-    .then(product => {
+  Product.findByPk(prodId)
+    .then((product) => {
       // console.log(products);
       // const product = products[0];
       // if (!product) {
       //   return res.redirect('/');
       // }
       console.log(product);
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        title: "Update Product"
+        title: "Update Product",
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -64,7 +65,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
   Product.findByPk(prodId)
     .then((product) => {
-      product.title= updatedTitle;
+      product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
@@ -73,13 +74,12 @@ exports.postEditProduct = (req, res, next) => {
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
-     
     })
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
- Product.findAll()
+  Product.findAll()
     // .getProducts()
     .then((products) => {
       res.render("admin/products", {
