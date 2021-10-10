d8798b393ca49d7d1855d1f8fc2f72ce00e76b82
Vaishnavi1601 <vaishnavijaiswal.18cs@saividya.ac.in>
2021-10-10 18:14:23 +0530

    working on cart item and order

--- models/user.js
+++ models/user.js
@@ -1,13 +1,15 @@
 const mongodb = require("mongodb");
-const getDb = require("../util/database").getDb; 
+const getDb = require("../util/database").getDb;
 
 // const ObjectId = mongodb.ObjectId;
 
 //creating user
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart,id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // cart is object with  array of  items {items: []}
+    this._id = id;
   }
 
   //saving user to db
@@ -17,13 +19,55 @@ class User {
     return db.collection("users").insertOne(this);
   }
 
+  addToCart(product) {
+    //here we get  the product we want to add to cart
+    // const cartProduct = this.cart.items.findIndex
+    //function that will be executed for every elemts in an item array
+    //cp is the product item in th eitem array
+    //(cp =>{
+    //return trur if found right product in items array
+    //   return cp._id === product._id;
+    //then we know that product already exists in the cart
+    //and then we need to add quantity for that
+
+    // });
+
+    //finout if cart contains certain product already
+
+    //firstky we assume there are no prduct sin the cxart
+    //so  we are adding product here w/o checking if product already exists
+
+    //updatedCart -- an object where we have items property which is an an array
+    //and will include product also the quantity
+
+    //...product -- pull out all the properties of product object and adding a new property i.e quantity:1
+
+    //updatedCart will create an object whoch holds an items property which is array of only one product
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb(); //aceessing db
+
+    //update the user to store that updatedCart
+    //update the user to add a rpoduct to the cart
+    return db
+      .collection("users") // users collection
+      .updatedOne(
+        // update one user there , with user id line 53
+        { _id: new ObjectId(this._id) }, //finding _id 
+        //and if we find that id we need to describe how to upadte
+        //we use $set , where we pass an object that holds info about which field to update in which way
+        //here cart which have user in db will receive updated cart
+        // so updatedCart will overwrite the old cart with new cart
+        { $set: { cart: updatedCart } }
+      );
+  }
+
   //finding user by userId
-  
   static findById(userId) {
     console.log(userId);
     const db = getDb();
     return (
-      db.collection("users")
+      db
+        .collection("users")
         // .find({_id: new mongodb.ObjectId(userId)})
         // .next()
         .findOne({ _id: mongodb.ObjectId(userId) })
@@ -36,7 +80,6 @@ class User {
         })
     );
   }
-
 
   // static findbyId(userId) {
   //   const db = getDb();
