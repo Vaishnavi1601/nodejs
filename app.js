3689bfe3099e734431aa0bdfd73afdac56801d23
Vaishnavi1601 <vaishnavijaiswal.18cs@saividya.ac.in>
2021-10-11 16:06:15 +0530

    adding and using a user model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose"); //importing mongoose
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,27 +20,27 @@ console.log("++++");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   //rech out to DB and  retrieve user with userid
-//   console.log(23, User);
-//   User.findById("61628506ea622e22df805643")
-//     .then((user) => {
-//       // console.log(25,user);
-//       // req.user = user; //when finding a user we store it in a request(req.user)
-//       // console.log(27,req.user); // usernwhich is stored here will be object with the properties we have in database
-//       // all methods of the user model will not be there,
-//       //bcz the user which are getting is of the databse ,
-//       // and methods aren't stored there
+app.use((req, res, next) => {
+  //rech out to DB and  retrieve user with userid
+  console.log(23, User);
+  User.findById("6164101e7fff168bcdcf08ea")
+    .then((user) => {
+      // console.log(25,user);
+      // req.user = user; //when finding a user we store it in a request(req.user)
+      // console.log(27,req.user); // usernwhich is stored here will be object with the properties we have in database
+      // all methods of the user model will not be there,
+      //bcz the user which are getting is of the databse ,
+      // and methods aren't stored there
 
-//       //So..to have a real user object with which we can interact,
-//       //we create a new user which allow us to work with whole user model and also with all the methods in user model
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
+      //So..to have a real user object with which we can interact,
+      //we create a new user which allow us to work with whole user model and also with all the methods in user model
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -53,8 +53,24 @@ mongoose
     "mongodb+srv://vaishnavi123:vaishnavi123@cluster0.7veks.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
-    app.listen(3000);
+    User.findOne().then((user) => {
+      if (!user) {
+
+        //creating new user 
+        const user = new User({
+          name: "vaishnavi",
+          email: "vaishnavi7734@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+
+        //this will be done when we start server
+        user.save();
+      }
+    });
+  app.listen(3000);
   })
-  .catch(err =>{
+  .catch((err) => {
     console.log(err);
-  })
+  });
