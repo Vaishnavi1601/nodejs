4bef800ef91fc9943c59508abd0b0aad4ba2e6f9
Vaishnavi1601 <vaishnavijaiswal.18cs@saividya.ac.in>
2021-10-13 02:38:50 +0530

    using mongoDB tos torec session

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);//this gives us function whixh should execute to which we pass our session
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const app = express();
 
+const MONGODB_URI = 'mongodb+srv://vaishnavi123:vaishnavi123@cluster0.7veks.mongodb.net/shop?retryWrites=true&w=majority';
+
+const app = express();
+const store = new MongoDBStore({
+  uri:MONGODB_URI,
+  collection: 'sessions', //defining collection where sesion will be stored
+
+});
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -26,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //secret -- used for signing the hash which secretly stores our ID in the cookie
 // resave:false-- means thaht the session will not be saved on every request that is done
 //saveUninitialized:false -- it ensures that no sessions gets saved for a request where it doesnt need to be saved
-aap.use(session({secret: 'my secret', resave:false, saveUninitialized: false}))
+app.use(session({secret: 'my secret', resave:false, saveUninitialized:false}))
 
 app.use((req, res, next) => {
   User.findById('6164101e7fff168bcdcf08ea')
@@ -45,7 +53,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://vaishnavi123:vaishnavi123@cluster0.7veks.mongodb.net/shop?retryWrites=true&w=majority'
+    MONGODB_URI
   )
   .then(result => {
     User.findOne().then(user => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,18 +3,23 @@ exports.getLogin = (req, res, next) => {
     //     .get("Cookie")
     //     .trim()
     //     .split("=")[1]);
-  const isLoggedIn = req
-  .get("Cookie")
-  .trim()
-  .split("=")[1];
+//   const isLoggedIn = req
+//   .get("Cookie")
+//   .trim()
+//   .split("=")[1];
+console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
-  });
+    isAuthenticated: false,
+    });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
-  res.redirect("/");
+
+    req.
+    // this session is added by session middleware
+    session.isLoggedIn= true; 
+
+    res.redirect("/");
 };
