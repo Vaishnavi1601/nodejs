f85cbe437792722dc58eeeb4df193e96e88e8de9
Vaishnavi1601 <vaishnavijaiswal.18cs@saividya.ac.in>
2021-10-14 18:43:23 +0530

    using mongodb to store session

--- app.js
+++ app.js
@@ -1,76 +1,88 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);//this gives us function whixh should execute to which we pass our session
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session); //this gives us function whixh should execute to which we pass our session
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-
-const MONGODB_URI = 'mongodb+srv://vaishnavi123:vaishnavi123@cluster0.7veks.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI =
+  "mongodb+srv://vaishnavi123:vaishnavi123@cluster0.7veks.mongodb.net/shop ";
 
 const app = express();
+
+//store doesnt know aboout mongooese model so when it fetches data from db it only fetches data not objects and method
 const store = new MongoDBStore({
-  uri:MONGODB_URI,
-  collection: 'sessions', //defining collection where sesion will be stored
-
+  uri: MONGODB_URI,
+  collection: "sessions", //defining collection where sesion will be stored
 });
-app.set('view engine', 'ejs');
-app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //intializing session middleware
-//configuring session setup in object 
+//configuring session setup in object
 //secret -- used for signing the hash which secretly stores our ID in the cookie
 // resave:false-- means thaht the session will not be saved on every request that is done
 //saveUninitialized:false -- it ensures that no sessions gets saved for a request where it doesnt need to be saved
-app.use(session({secret: 'my secret', resave:false, saveUninitialized:false}))
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
+//storing our user in request
 app.use((req, res, next) => {
-  User.findById('6164101e7fff168bcdcf08ea')
-    .then(user => {
+  if (!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
+    .then((user) => {
+      console.log(57, user);
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    MONGODB_URI
-  )
-  .then(result => {
-    User.findOne().then(user => {
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: 'alexbob',
-          email: 'max@test.com',
+          name: "alexbob",
+          email: "max@test.com",
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
     });
     app.listen(3000);
-    console.log('CONNECTED======')
+    console.log("CONNECTED======");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,27 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    // console.log(req
-    //     .get("Cookie")
-    //     .trim()
-    //     .split("=")[1]);
-//   const isLoggedIn = req
-//   .get("Cookie")
-//   .trim()
-//   .split("=")[1];
-console.log(req.session.isLoggedIn);
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: false,
-    });
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
 };
 
 exports.postLogin = (req, res, next) => {
+    console.log(12,User)
+  User.findById('6164101e7fff168bcdcf08ea')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;  //
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
 
-    req.
-    // this session is added by session middleware
-    session.isLoggedIn= true; 
-
-    res.redirect("/");
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
